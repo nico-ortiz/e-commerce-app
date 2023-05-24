c01e4e5bbd3650312968eae2a9a640a2a0fff8ff
Nicolas-Ortiz <nicortiz1234@gmail.com>
2023-05-24 19:14:48 -0300

    Add a simple endpoint

--- src/main/java/com/project/ecommerce/EcommerceApplication.java
+++ src/main/java/com/project/ecommerce/EcommerceApplication.java
@@ -2,12 +2,20 @@ package com.project.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String hello(@RequestParam("name") String name) {
+		return String.format("Helloooooo %s!", name);
+	}
 }
